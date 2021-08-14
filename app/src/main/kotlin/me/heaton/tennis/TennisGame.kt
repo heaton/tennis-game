7e0469f4cfa1905df97df02bd8317372f37ec89b
heaton.cai <heaton.cai@myob.com>
2021-08-15 01:55:14 +1000

    make all the private methods idempotent

--- app/src/main/kotlin/me/heaton/tennis/TennisGame.kt
+++ app/src/main/kotlin/me/heaton/tennis/TennisGame.kt
@@ -6,21 +6,25 @@ class TennisGame(private val firstPlayer: String, private val secondPlayer: Stri
     private var scoreEvents = mutableListOf<String>()
     private val scoreHistory = mutableListOf(score())
 
-    private fun firstPlayerPoint(): Int = points().first
+    private fun firstPlayerPoint(): Int = points(scoreEvents).first
 
-    private fun secondPlayerPoint(): Int = points().second
+    private fun secondPlayerPoint(): Int = points(scoreEvents).second
 
-    fun score(): String =
+    fun score(): String = score(Pair(firstPlayerPoint(), secondPlayerPoint()))
+
+    private fun score(points: Pair<Int, Int>) = with(points) {
         when {
-            afterDeuce() && firstPlayerPoint() == secondPlayerPoint() -> "deuce"
-            afterDeuce() && abs(firstPlayerPoint() - secondPlayerPoint()) == 1 -> "advantage ${betterPlayer()}"
-            firstPlayerPoint() > 3 || secondPlayerPoint() > 3 -> "${betterPlayer()} wins"
-            else -> "${scoreOf(firstPlayerPoint())}, ${scoreOf(secondPlayerPoint())}"
+            afterDeuce(points) && first == second -> "deuce"
+            afterDeuce(points) && abs(first - second) == 1 -> "advantage ${betterPlayer(points)}"
+            first > 3 || second > 3 -> "${betterPlayer(points)} wins"
+            else -> "${scoreOf(first)}, ${scoreOf(second)}"
         }
+    }
 
-    private fun betterPlayer(): String = if (firstPlayerPoint() > secondPlayerPoint()) firstPlayer else secondPlayer
+    private fun betterPlayer(points: Pair<Int, Int>): String =
+        if (points.first > points.second) firstPlayer else secondPlayer
 
-    private fun afterDeuce() = firstPlayerPoint() > 2 && secondPlayerPoint() > 2
+    private fun afterDeuce(points: Pair<Int, Int>) = points.first > 2 && points.second > 2
 
     private fun scoreOf(point: Int) = when (point) {
         0 -> "love"
@@ -35,7 +39,7 @@ class TennisGame(private val firstPlayer: String, private val secondPlayer: Stri
         scoreHistory.add(score())
     }
 
-    private fun points() = scoreEvents.fold(Pair(0, 0), ::addScoreForPlayer)
+    private fun points(events: List<String>) = events.fold(Pair(0, 0), ::addScoreForPlayer)
 
     private fun addScoreForPlayer(previousScore: Pair<Int, Int>, player: String) = with(previousScore) {
         if (player == firstPlayer) Pair(first + 1, second) else Pair(first, second + 1)
