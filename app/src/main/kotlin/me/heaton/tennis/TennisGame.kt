ab4fb023b4fd8f08901a3d2edde3747f57f61a70
heaton.cai <heaton.cai@myob.com>
2021-08-15 00:30:06 +1000

    create functions to point access

--- app/src/main/kotlin/me/heaton/tennis/TennisGame.kt
+++ app/src/main/kotlin/me/heaton/tennis/TennisGame.kt
@@ -3,21 +3,24 @@ package me.heaton.tennis
 import kotlin.math.abs
 
 class TennisGame(private val firstPlayer: String, private val secondPlayer: String) {
-    private var firstPlayerPoint = 0
-    private var secondPlayerPoint = 0
+    private var _firstPlayerPoint = 0
+    private var _secondPlayerPoint = 0
     private val scoreHistory = mutableListOf(score())
+
+    private fun firstPlayerPoint() = _firstPlayerPoint
+    private fun secondPlayerPoint() = _secondPlayerPoint
 
     fun score(): String =
         when {
-            afterDeuce() && firstPlayerPoint == secondPlayerPoint -> "deuce"
-            afterDeuce() && abs(firstPlayerPoint - secondPlayerPoint) == 1 -> "advantage ${betterPlayer()}"
-            firstPlayerPoint > 3 || secondPlayerPoint > 3 -> "${betterPlayer()} wins"
-            else -> "${scoreOf(firstPlayerPoint)}, ${scoreOf(secondPlayerPoint)}"
+            afterDeuce() && firstPlayerPoint() == secondPlayerPoint() -> "deuce"
+            afterDeuce() && abs(firstPlayerPoint() - secondPlayerPoint()) == 1 -> "advantage ${betterPlayer()}"
+            firstPlayerPoint() > 3 || secondPlayerPoint() > 3 -> "${betterPlayer()} wins"
+            else -> "${scoreOf(firstPlayerPoint())}, ${scoreOf(secondPlayerPoint())}"
         }
 
-    private fun betterPlayer(): String = if (firstPlayerPoint > secondPlayerPoint) firstPlayer else secondPlayer
+    private fun betterPlayer(): String = if (firstPlayerPoint() > secondPlayerPoint()) firstPlayer else secondPlayer
 
-    private fun afterDeuce() = firstPlayerPoint > 2 && secondPlayerPoint > 2
+    private fun afterDeuce() = firstPlayerPoint() > 2 && secondPlayerPoint() > 2
 
     private fun scoreOf(point: Int) = when (point) {
         0 -> "love"
@@ -28,13 +31,13 @@ class TennisGame(private val firstPlayer: String, private val secondPlayer: Stri
 
     fun firstPlayerScores() {
         if ("wins" in score()) throw GameEndedException()
-        firstPlayerPoint++
+        _firstPlayerPoint++
         scoreHistory.add(score())
     }
 
     fun secondPlayerScores() {
         if ("wins" in score()) throw GameEndedException()
-        secondPlayerPoint++
+        _secondPlayerPoint++
         scoreHistory.add(score())
     }
 
